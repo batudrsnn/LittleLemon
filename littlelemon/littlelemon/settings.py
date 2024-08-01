912c30f16045ddf38a1a8749daf2d12728a54acc
Batuhan Dursun <yusa.batuhan.dursun@gmail.com>
2024-08-01 19:17:33 +0200

    API endpoints are created

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+     'DEFAULT_AUTHENTICATION_CLASSES': (
+         'rest_framework.authentication.SessionAuthentication',
+     ),
+
+ }
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from .models import Menu
+from .serializer import MenuSerializer
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAdminUser]
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAdminUser]
+    
