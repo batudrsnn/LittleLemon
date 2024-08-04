f5e34e7545613a9a06b5acac90ce57a569fe2efb
Batuhan Dursun <yusa.batuhan.dursun@gmail.com>
2024-08-04 10:33:32 +0200

    Test module is created

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -15,5 +15,5 @@ class Menu(models.Model):
     Price = models.DecimalField(max_digits=2, decimal_places=2)
     Inventory = models.IntegerField()
 
-    def __str__(self) -> str:
-        return self.Title
+    def __str__(self):
+        return f'{self.Title} : {str(self.Price)}'
